1609722e9b6d3fb2717e0a0582830ccd58e1fb2e
Daffa Rihadatul Fairus <hanura423@gmail.com>
2024-11-02 06:54:52 +0700

    first Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import bodyParser from "body-parser";
+import express from "express";
+import expressEjsLayouts from "express-ejs-layouts";
+
+
+
+const app = express();
+const port = 3000;
+
+// set static 
+app.use(express.static("public"));
+// data post
+app.use(bodyParser.urlencoded({ extended: true }));
+// set template engine
+app.use(expressEjsLayouts);
+app.set('layout', './layouts/full-width');
+app.set("view engine", "ejs");
+
+
+// rendering viwe
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: "LMS Forsk College"
+  }); // pastikan file index.ejs memiliki konten
+});
+app.get('/add', (req,res) => {
+  res.render('add', {
+    title: "LMS Forsk College",
+    layout: "./layouts/add"
+  })
+})
+app.get("/update", (req,res) => {
+  res.render("update", {
+    title: "LMS Forsk College",
+    layout: "./layouts/edit",
+  })
+})
+
+app.listen(port, (req, res) => {
+  console.log(`Listening Port ${port}`);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./views/**/*.ejs",
+    "./public/**/*.css",
+    "./public/**/*.js", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
+
